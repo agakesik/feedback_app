c5e77506750a5acc1dd5fb91fb10ccb03c9500a8
Aga <aga.k396@gmail.com>
2020-03-26 16:31:21 +0100

    Set up first mockup of Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/help'
+  get 'pages/home'
+  get 'pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'pages#home'
 end
