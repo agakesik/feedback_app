d9a27c9332fd9153d5040ec206bfab4c2002037f
Aga <aga.k396@gmail.com>
2020-03-27 11:38:22 +0100

    Add test for layout

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_url
     assert_response :success
   end
 
   test "should get help" do
-    get pages_help_url
+    get help_path
     assert_response :success
   end
 
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test 'layout content' do
+    get root_path
+    assert_template 'pages/home'
+    assert_select "div.content-container"
+    assert_select "div.sidebar"
+    assert_select "a[href=?]", root_url, count: 1
+    assert_select "a[href=?]", help_path, count: 1
+  end
+end
