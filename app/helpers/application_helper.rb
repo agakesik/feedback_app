238bb43a8f6a985e95f796a68e362d0bd3d90c76
Aga <aga.k396@gmail.com>
2020-04-06 09:37:00 +0200

    Unify page type class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
 
+  # Returns page type: spreadshit or single page
+  def page_type(page_type = '')
+    if page_type.empty?
+      "single-page"
+    else
+      "spreadsheet-page"
+    end
+  end
+
 end
