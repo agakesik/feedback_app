b46ecaabc30232b3c3fb8986be16050cfe08f60f
Aga <aga.k396@gmail.com>
2020-04-24 15:38:11 +0200

    Looking for error in deployment #6

--- db/migrate/20200421112040_add_email_to_users.rb
+++ db/migrate/20200421112040_add_email_to_users.rb
@@ -1,6 +1,6 @@
 class AddEmailToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :email, :string
-    add_index :users, :email, unique: true
+    add_index :users, :email
   end
 end
