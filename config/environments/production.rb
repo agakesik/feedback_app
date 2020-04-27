543e9c46bac1a44cb26ecaf7d87547564507f350
Aga <aga.k396@gmail.com>
2020-04-27 10:43:41 +0200

    Fix error in convig/production in setting up mailer - trying to set gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,16 +66,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://rd-feedback-app.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'rd-feedback-app.heroku.com',
-    :authentication => :plain,
-  }
+  # host = 'https://rd-feedback-app.herokuapp.com'
+  config.action_mailer.smtp_settings = {
+
+       :address => "smtp.gmail.com",
+       :port => 587,
+       :user-name => "GMAIL_ID",
+       :password => "GMAIL_PASSWORD",
+       :authentication => :plain,
+       :enable_starttls_auto => true
+
+}
   ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
