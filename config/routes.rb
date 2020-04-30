3119d59aa571d2b74b3cfe1828af5d98920d3be5
Aga <aga.k396@gmail.com>
2020-04-30 10:22:53 +0200

    Fix user show layout for mobile and add link to #ratings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get '/help', to: 'pages#help'
-  get '/menu', to: 'pages#menu'
   get '/admin', to: 'pages#admin'
   resources :users
   resources :users do
