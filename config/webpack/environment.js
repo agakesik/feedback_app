a6a13b4249d3d7b98ae07dd06af409f9ecebe0b1
agakesik <aga.k396@gmail.com>
2021-12-01 15:43:43 +0100

    upgrade gems and webpacker config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,14 @@
-const { environment } = require('@rails/webpacker')
-const coffee =  require('./loaders/coffee')
+const { environment } = require("@rails/webpacker");
+const coffee = require("./loaders/coffee");
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-new webpack.ProvidePlugin({
-  $: 'jquery/src/jquery',
-  jQuery: 'jquery/src/jquery'
-})
-)
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+  })
+);
 
-environment.loaders.prepend('coffee', coffee)
-module.exports = environment
+environment.loaders.prepend("coffee", coffee);
+module.exports = environment;
